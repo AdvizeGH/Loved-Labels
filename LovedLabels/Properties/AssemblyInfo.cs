99e588e1eb1895f6997939464b67616bd93ff897
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-07-26 20:13:53 -0400

    update mod build package & migrate to new .csproj format
    The new package version adds support for SMAPI 3.0 and Stardew Valley 1.4, and deploys 'assets' folders automatically. This commit also sets the target platform to x86, to avoid mismatched processor architecture warnings since the game is 32-bit.

--- LovedLabels/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("LovedLabels")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("2.2.1")]
-[assembly: AssemblyFileVersion("2.2.1")]
